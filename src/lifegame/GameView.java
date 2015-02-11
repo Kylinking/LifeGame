19d044d818f1537ac80791c19e383c91037ec375
kylinking <kylinsmail@gmail.com>
2015-02-11 17:59:05 +0800

    Add block select feature
    choose the primtive state of the game by click mouse

---
+++ src/lifegame/GameView.java
@@ -0,0 +1,148 @@
+package lifegame;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.*;
+
+import javax.swing.*;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class GameView {
+
+	private Game game;
+	private JFrame frame;
+	private JPanel initPanel, buttonPanel, gamePanel;
+	private JButton buttonStart, buttonRestart;
+	private Timer timer;
+
+	public GameView() {
+		timer = new Timer();
+		frame = new JFrame("Life Game");
+		Dimension maxmumsize = new Dimension();
+		maxmumsize.setSize(1280, 1280);
+		initPanel = new InitPanel();
+		frame.setSize(600, 700);
+		frame.add(initPanel, BorderLayout.CENTER);
+		buttonStart = new JButton("Start");
+		buttonRestart = new JButton("Default Demo");
+		buttonPanel = new JPanel();
+		buttonPanel.add(buttonStart);
+		buttonPanel.add(buttonRestart);
+		frame.add(buttonPanel,BorderLayout.SOUTH);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+}
+
+@SuppressWarnings("serial")
+class InitPanel extends JPanel implements MouseListener 
+{
+
+	private ArrayList<Integer> selected;
+
+	public InitPanel() {
+		this.setSize(600, 800);
+		addMouseListener(this);
+		selected = new ArrayList<Integer>();
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		int x, y;
+		for (int i = 0; i != 64; i++) {
+			x = i * 20;
+			for (int j = 0; j != 64; j++) {
+				y = j * 20;
+				g.setColor(Color.gray);
+				g.fillRect(x, y, 20, 20);
+				g.setColor(Color.black);
+				g.drawRect(x, y, 20, 20);
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		int x = arg0.getX() / 20;
+		int y = arg0.getY() / 20;
+		Graphics g = this.getGraphics();
+		g.setColor(Color.black);
+		g.fillRect(x * 20, y * 20, 20, 20);
+		System.out.println("x: " + x + " y: " + y);
+		this.selected.add(y * 64 + x);
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ArrayList<Integer> GetSeletectRect() {
+		return this.selected;
+	}
+}
+
+@SuppressWarnings("serial")
+class GamePanel extends JPanel 
+{
+	private Game game;
+
+	public GamePanel(Game game) {
+		this.game = game;
+		this.setSize(600, 800);
+		this.setVisible(false);
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.clearRect(0, 0, getWidth(), getHeight());
+		g.setColor(Color.black);
+		for (int i = 0; i != game.GetLives().length; i++) {
+			int x = i % 64 * 10;
+			int y = i / 64 * 10;
+			if (game.GetLives()[i].isLive()) {
+				g.fillRect(x, y, 10, 10);
+			}
+		}
+	}
+}
+
+class StartButton extends JButton
+{
+	ArrayList<Integer> list;
+	private Game game;
+	
+	public StartButton(Game game, ArrayList<Integer> list)
+	{
+		this.list = list;
+	}
+	@Override
+	public void doClick()
+	{
+		
+	}
+}
--- src/lifegame/UI.java
+++ src/lifegame/UI.java
@@ -1,72 +1,78 @@
-package lifegame;
-
-import java.awt.Color;
-import java.awt.Graphics;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
-
-import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
-
-public class UI {
-
-	Game game;
-	JFrame frame;
-	JPanel pane;
-
-	@SuppressWarnings("serial")
-	public static void main(String[] args) {
-		
-		Timer timer = new Timer();
-
-		final UI GameUI = new UI();	
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		for (int i=0;i<20;i++){
-			list.add(i);
-		}
-		GameUI.game = Game.Initialize(list);
-		
-		GameUI.frame = new JFrame("LifeGame");
-		GameUI.frame.setSize(640, 640);
-		GameUI.frame.setVisible(true);
-		GameUI.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-		GameUI.pane = new JPanel() {
-			public void paint(Graphics g) {
-				super.paint(g);
-				//Life[] lives = GameUI.game.GetLives();
-				g.clearRect(0, 0, getWidth(), getHeight());
-				g.setColor(Color.BLUE);
-				for (int i = 0; i != GameUI.game.GetLives().length; i++) {
-					int x = i % 64 * 10;
-					int y = i / 64 * 10;
-					if (GameUI.game.GetLives()[i].isLive()) {
-						g.fillRect(x, y, 10, 10);
-					}
-				}				
-			}
-		};
-		
-		GameUI.frame.add(GameUI.pane);
-		timer.schedule(new TimerTaskTest(GameUI.pane,GameUI.game), 3000, 1000);
-	}
-
-	static class TimerTaskTest extends TimerTask {
-		JPanel panel;
-		Game game;
-		TimerTaskTest(JPanel panel,Game game) {
-			this.panel = panel;
-			this.game = game;
-		}
-
-		@Override
-		public void run() {
-			game.NextGeneratation();
-			panel.repaint();
-		}
-
-	}
-}
+//package lifegame;
+//
+//import java.awt.BorderLayout;
+//import java.awt.Color;
+//import java.awt.Graphics;
+//
+//import javax.swing.*;
+//
+//
+//import java.util.ArrayList;
+//import java.util.Timer;
+//import java.util.TimerTask;
+//
+//public class UI {
+//
+//	Game game;
+//	JFrame frame;
+//	JPanel pane,southPane;
+//	JButton buttonStart,buttonRestart;
+//
+//	@SuppressWarnings("serial")
+//	public static void main(String[] args) {
+//		
+//		Timer timer = new Timer();
+//
+//		final UI GameUI = new UI();	
+//		ArrayList<Integer> list = new ArrayList<Integer>();
+//		for (int i=0;i<20;i++){
+//			list.add(i);
+//		}
+//		GameUI.game = Game.Initialize(list);
+//		
+//		GameUI.frame = new JFrame("LifeGame");
+//		GameUI.frame.setSize(640, 640);
+//		GameUI.frame.setVisible(true);
+//		GameUI.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//
+//		GameUI.pane = new JPanel() {
+//			public void paint(Graphics g) {
+//				super.paint(g);
+//				g.clearRect(0, 0, getWidth(), getHeight());
+//				g.setColor(Color.BLUE);
+//				for (int i = 0; i != GameUI.game.GetLives().length; i++) {
+//					int x = i % 64 * 10;
+//					int y = i / 64 * 10;
+//					if (GameUI.game.GetLives()[i].isLive()) {
+//						g.fillRect(x, y, 10, 10);
+//					}
+//				}				
+//			}
+//		};
+//		GameUI.buttonStart = new JButton("开始");
+//		GameUI.buttonRestart = new JButton("重新开始");
+//		GameUI.southPane = new JPanel();
+//		GameUI.southPane.add(GameUI.buttonStart);
+//		GameUI.southPane.add(GameUI.buttonRestart);
+//		GameUI.frame.add(GameUI.pane,BorderLayout.CENTER);
+//		GameUI.frame.add(GameUI.southPane,BorderLayout.SOUTH);
+//		
+//		timer.schedule(new TimerTaskTest(GameUI.pane,GameUI.game), 3000, 1000);
+//	}
+//
+//	static class TimerTaskTest extends TimerTask {
+//		JPanel panel;
+//		Game game;
+//		TimerTaskTest(JPanel panel,Game game) {
+//			this.panel = panel;
+//			this.game = game;
+//		}
+//
+//		@Override
+//		public void run() {
+//			game.NextGeneratation();
+//			panel.repaint();
+//		}
+//
+//	}
+//}
